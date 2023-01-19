2ed20c7ec3f75734b14433981016bde45c247972
Paulo Matias <matias@ufscar.br>
2023-01-19 14:51:05 -0300

    updsample: close db after getting ids

--- lattesmachine/updsample.py
+++ lattesmachine/updsample.py
@@ -7,20 +7,20 @@ from .extract import extract
 logger = logging.getLogger(__name__)
 
 
-def cv_ids(db):
+def cv_ids(db_path):
+    db = rocksdb.DB(db_path, rocksdb.Options(), read_only=True)
     it = db.iterkeys()
     it.seek_to_first()
-    return list(it)
-
-
-def updsample(db, probability):
-    ids = cv_ids(db)
-    sample = random.sample(ids, int(probability*len(ids)))
-    logger.info('Atualizando %d CVs', len(sample))
-    extract(db, [{'idcnpq': idcnpq} for idcnpq in sample])
+    res = list(it)
+    db.close()
+    return res
 
 
 def updsample_cmd(db_path, probability):
+    ids = cv_ids(db_path)
+    sample = random.sample(ids, int(probability*len(ids)))
+    logger.info('Atualizando %d CVs', len(sample))
+
     db = rocksdb.DB(db_path, rocksdb.Options(compression=rocksdb.CompressionType.lz4_compression))
-    updsample(db, probability)
+    extract(db, [{'idcnpq': idcnpq} for idcnpq in sample])
     db.close()
